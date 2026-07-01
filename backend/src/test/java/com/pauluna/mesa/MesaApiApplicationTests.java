b8fdf28502a21948d182590e38e19608ec6cf09b
paula <paulirei@gmail.com>
2026-07-01 09:30:34 +0200

    chore: initialize Spring Boot backend

---
+++ backend/src/test/java/com/pauluna/mesa/MesaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauluna.mesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
