1c13a8c5fdde344cf50aca65bd766bdfdf26438d
Paula <102218925+PaulaReixach@users.noreply.github.com>
2026-07-08 22:44:29 +0200

    feat: add group activity event types

---
+++ backend/src/main/java/com/pauluna/mesa/group/domain/GroupActivityType.java
@@ -0,0 +1,11 @@
+package com.pauluna.mesa.group.domain;
+
+public enum GroupActivityType {
+    GROUP_CREATED,
+    MEMBER_INVITED,
+    MEMBER_JOINED,
+    MEMBER_LEFT,
+    RESTAURANT_ADDED,
+    RESTAURANT_RATED,
+    RESTAURANT_STATUS_CHANGED
+}
